83d874c10eda3c8763cf83a84b3f526672dd4292
LAB11_USER ON WS29 <L11Y08W29@citulab.net>
2024-04-04 10:08:46 +0800

    added connect3

---
+++ AndroidProjectCollection/app/src/main/java/com/example/androidprojectcollection/Connect3.java
@@ -0,0 +1,15 @@
+package com.example.androidprojectcollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Connect3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect3);
+
+    }
+}
